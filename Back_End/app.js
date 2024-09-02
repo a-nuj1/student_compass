4da62bb026d35617c1d604b3e16ac1e29842f1df
Anuj <anujgupta1532003@gmail.com>
2024-09-02 20:09:07 +0530

    default route

--- Back_End/app.js
+++ Back_End/app.js
@@ -35,4 +35,8 @@ app.use('/api/v1', other);
 
 export default app;
 
+app.get("/", (req, res) => {
+    res.send(`<h1> Site is Working...! Click<a href = ${process.env.FRONTEND_URL}>here</a> to Visit Frontend.</h1>`);
+});
+
 app.use(errorHandler)
